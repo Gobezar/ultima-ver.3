9be1fbb857083721b8d5c3411af413ed43893f2a
Gobezar <105110053+Gobezar@users.noreply.github.com>
2022-08-23 17:04:09 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,113 @@
+function readMore() {
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var service_btn = document.getElementById("service_btn");
+
+
+if(dots.style.display === "none") {
+    dots.style.display="inline";
+    service_btn.innerHTML="Подробнее";
+    more.style.display="none";
+}   
+    else {
+    dots.style.display="none";
+    service_btn.innerHTML="Скрыть";
+    more.style.display="inline";
+
+}
+}
+
+function readMore2() {
+    var dots = document.getElementById("dots2");
+    var more = document.getElementById("more2");
+    var service_btn = document.getElementById("service_btn2");
+
+
+if(dots2.style.display === "none") {
+    dots2.style.display="inline";
+    service_btn2.innerHTML="Подробнее";
+    more2.style.display="none";
+}   
+    else {
+    dots2.style.display="none";
+    service_btn2.innerHTML="Скрыть";
+    more2.style.display="inline";
+
+}
+}
+
+function readMore3() {
+    var dots = document.getElementById("dots3");
+    var more = document.getElementById("more3");
+    var service_btn = document.getElementById("service_btn3");
+
+
+if(dots3.style.display === "none") {
+    dots3.style.display="inline";
+    service_btn3.innerHTML="Подробнее";
+    more3.style.display="none";
+}   
+    else {
+    dots3.style.display="none";
+    service_btn3.innerHTML="Скрыть";
+    more3.style.display="inline";
+
+}
+}
+
+function readMore4() {
+    var dots = document.getElementById("dots4");
+    var more = document.getElementById("more4");
+    var service_btn = document.getElementById("service_btn4");
+
+
+if(dots4.style.display === "none") {
+    dots4.style.display="inline";
+    service_btn4.innerHTML="Подробнее";
+    more4.style.display="none";
+}   
+    else {
+    dots4.style.display="none";
+    service_btn4.innerHTML="Скрыть";
+    more4.style.display="inline";
+
+}
+}
+
+function readMore5() {
+    var dots = document.getElementById("dots5");
+    var more = document.getElementById("more5");
+    var service_btn = document.getElementById("service_btn5");
+
+
+if(dots5.style.display === "none") {
+    dots5.style.display="inline";
+    service_btn5.innerHTML="Подробнее";
+    more5.style.display="none";
+}   
+    else {
+    dots5.style.display="none";
+    service_btn5.innerHTML="Скрыть";
+    more5.style.display="inline";
+
+}
+}
+
+function readMore6() {
+    var dots = document.getElementById("dots6");
+    var more = document.getElementById("more6");
+    var service_btn = document.getElementById("service_btn6");
+
+
+if(dots6.style.display === "none") {
+    dots6.style.display="inline";
+    service_btn6.innerHTML="Подробнее";
+    more6.style.display="none";
+}   
+    else {
+    dots6.style.display="none";
+    service_btn6.innerHTML="Скрыть";
+    more6.style.display="inline";
+
+}
+}
